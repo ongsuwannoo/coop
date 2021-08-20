4d567418faef7cb1802a74fe2d6de750cb8ee824
ongsuwannoo <ongsuwannoo@Gmail.com>
2021-08-20 12:39:38 +0200

    (gitaction): 🚀 Add gitaction workflows check-code.yaml

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const app = express();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+app.get("/health", (req, res) => {
+  res.send("respond with a resource");
 });
-
-module.exports = router;
+module.exports = app;
